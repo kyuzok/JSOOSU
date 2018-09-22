b79027ff94df82a80f705e0b911282917eafa4ee
VisshaalS <40528586+VisshaalS@users.noreply.github.com>
2018-09-21 23:24:32 -0400

    Buttons

--- app/src/main/java/com/spicystar/jsoosu/AboutUs.java
+++ app/src/main/java/com/spicystar/jsoosu/AboutUs.java
@@ -1,13 +1,30 @@
 package com.spicystar.jsoosu;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class AboutUs extends AppCompatActivity {
+
+    Button bToMain;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_us);
+
+        bToMain = findViewById(R.id.bToMain);
+        bToMain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent aboutToMain = new Intent(AboutUs.this,MainActivity.class);
+                AboutUs.this.startActivity(aboutToMain);
+            }
+        });
+
+
+
     }
 }
--- app/src/main/java/com/spicystar/jsoosu/MainActivity.java
+++ app/src/main/java/com/spicystar/jsoosu/MainActivity.java
@@ -1,13 +1,33 @@
 package com.spicystar.jsoosu;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    Button bToAbout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        bToAbout = findViewById(R.id.bToAbout);
+
+        bToAbout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent mainToAbout = new Intent(MainActivity.this,AboutUs.class);
+                MainActivity.this.startActivity(mainToAbout);
+            }
+        });
+
+
+
     }
 }
