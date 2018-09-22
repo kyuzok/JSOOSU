b32852b69ec744bc0e12345ec5efb6d41d0ab70f
VisshaalS <40528586+VisshaalS@users.noreply.github.com>
2018-09-21 23:27:27 -0400

    Buttons

--- app/src/main/java/com/spicystar/jsoosu/AboutUs.java
+++ app/src/main/java/com/spicystar/jsoosu/AboutUs.java
@@ -21,6 +21,7 @@ public class AboutUs extends AppCompatActivity {
             public void onClick(View v) {
                 Intent aboutToMain = new Intent(AboutUs.this,MainActivity.class);
                 AboutUs.this.startActivity(aboutToMain);
+                
             }
         });
 
