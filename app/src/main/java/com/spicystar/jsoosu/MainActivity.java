f4688f87edacfaf01c49efe83babadb481fdd266
Kyuzo Kelly <tekitoh.otoko@gmail.com>
2018-12-17 22:35:30 -0500

    BottomNav & Fragments

--- app/src/main/java/com/spicystar/jsoosu/MainActivity.java
+++ app/src/main/java/com/spicystar/jsoosu/MainActivity.java
@@ -1,33 +1,59 @@
 package com.spicystar.jsoosu;
 
+import android.support.v4.app.Fragment;
 import android.content.Intent;
+import android.provider.CalendarContract;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
 
 public class MainActivity extends AppCompatActivity {
 
-    Button bToAbout;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-        bToAbout = findViewById(R.id.bToAbout);
-
-        bToAbout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent mainToAbout = new Intent(MainActivity.this,AboutUs.class);
-                MainActivity.this.startActivity(mainToAbout);
-            }
-        });
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_nav);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
 
 
 
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment selectedFragment = null;
+
+
+                switch (item.getItemId()) {
+                    case R.id.nav_events:
+                        selectedFragment = new EventsFragment();
+                        break;
+                    case R.id.nav_officers:
+                        selectedFragment = new OfficersFragment();
+                        break;
+                    case R.id.nav_photos:
+                        selectedFragment = new PhotosFragment();
+                        break;
+                    case R.id.nav_more:
+                        selectedFragment = new MoreFragment();
+                        break;
+                }
+
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        selectedFragment).commit();
+                return true;
+                }
+            };
+
+
 }
