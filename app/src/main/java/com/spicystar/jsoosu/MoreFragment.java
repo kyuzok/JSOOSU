4c0ae80f14e1514055c692659bf102420b01365a
Kyuzo Kelly <tekitoh.otoko@gmail.com>
2019-05-14 16:47:10 -0400

    trying to move Login to new activity

--- app/src/main/java/com/spicystar/jsoosu/MoreFragment.java
+++ app/src/main/java/com/spicystar/jsoosu/MoreFragment.java
@@ -16,9 +16,6 @@ import android.text.TextUtils;
 
 public class MoreFragment extends Fragment {
 
-    private EditText Name;
-    private EditText Password;
-    private Button Login;
 
 
     @Nullable
@@ -26,48 +23,11 @@ public class MoreFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
         View v = inflater.inflate(R.layout.fragment_more, container, false);
-        Name = v.findViewById(R.id.etName);
-        Password = v.findViewById(R.id.etPass);
-        Login = v.findViewById(R.id.btnLogin);
 
-        Login.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String name = Name.getText().toString().trim();
-                String pass = Password.getText().toString().trim();
-
-                if (TextUtils.isEmpty(name) || TextUtils.isEmpty(pass)) {
-                    Toast.makeText(getActivity(),"At least one box is empty", Toast.LENGTH_SHORT).show();
-                }
-                else {
-                    validate(name, pass);
-                }
-            }
-        });
 
         return v;
 
     }
 
-    private void validate(String userName, String userPassword) {
-        if ((userName.equals("JSO")) && (userPassword.equals("634556"))) {
-            //shows that the login is successful in a message then moves to a newActivity
-            Toast.makeText(getActivity(),"Login Successful", Toast.LENGTH_SHORT).show();
-            moveToNewActivity();
-        }
-        else {
-            //error message saying that the login was unsuccessful
-            Toast.makeText(getActivity(),"Login Unsuccessful", Toast.LENGTH_SHORT).show();
-        }
-
-    }
-
-    private void moveToNewActivity () {
-
-        Intent i = new Intent(getActivity(), AboutUs.class);
-        startActivity(i);
-        getActivity();
-
-    }
 
 }
