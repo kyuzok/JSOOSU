8a9c0a78962c820b355e118690a8dd9353fbe799
VisshaalS <40528586+visshaals@users.noreply.github.com>
2019-05-15 20:47:43 -0400

    DeleteEventActivity should be working
    Signed-off-by: VisshaalS <40528586+VisshaalS@users.noreply.github.com>

--- app/src/main/java/com/spicystar/jsoosu/EventDeleteActivity.java
+++ app/src/main/java/com/spicystar/jsoosu/EventDeleteActivity.java
@@ -2,12 +2,71 @@ package com.spicystar.jsoosu;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.List;
 
 public class EventDeleteActivity extends AppCompatActivity {
+
+    private FirebaseFirestore db;
+    private LinearLayout linear;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event_delete);
+
+        linear = findViewById(R.id.linear);
+
+        db = FirebaseFirestore.getInstance();
+
+
+
+        db.collection("Events").get().addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+            @Override
+            public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+                if(!queryDocumentSnapshots.isEmpty()){
+                    List<DocumentSnapshot> list = queryDocumentSnapshots.getDocuments();
+                    for(DocumentSnapshot d: list){
+                        String name = d.getString("Name");
+
+                        buttons(name);
+
+                    }
+
+                }
+            }
+        });
+
+
     }
+
+    private void buttons(final String name) {
+
+        //create the buttons to show up for each document
+        Button btn = new Button(this);
+        btn.setText(name);
+        btn.setAllCaps(false);
+        linear.addView(btn);
+
+        btn.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                //removes button from view
+                linear.removeView(v);
+                //removes the data from database
+                db.collection("Events").document(name).delete();
+                return true;
+            }
+        });
+
+    }
+
 }
