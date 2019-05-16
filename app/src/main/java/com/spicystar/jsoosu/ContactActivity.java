10c1781fcf94f5ddae34cb2dcca7eabbf78eb97e
Kyuzo Kelly <tekitoh.otoko@gmail.com>
2019-05-15 20:29:20 -0400

    email function complete

--- app/src/main/java/com/spicystar/jsoosu/ContactActivity.java
+++ app/src/main/java/com/spicystar/jsoosu/ContactActivity.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+import android.os.Bundle;
 
 public class ContactActivity extends AppCompatActivity {
 
