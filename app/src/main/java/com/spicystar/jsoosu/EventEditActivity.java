d0183879c6ebc786ad3ec84d483365391766050d
Kyuzo Kelly <tekitoh.otoko@gmail.com>
2019-05-20 16:42:25 -0400

    copied the addEvent template to editEvent tried editing EventsFragment

--- app/src/main/java/com/spicystar/jsoosu/EventEditActivity.java
+++ app/src/main/java/com/spicystar/jsoosu/EventEditActivity.java
@@ -2,12 +2,114 @@ package com.spicystar.jsoosu;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TimePicker;
+import android.widget.Toast;
 
-public class EventEditActivity extends AppCompatActivity {
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/*
+*Qzo's idea for implementing this method:
+* take the information that's set in firebase for this event:
+* display that in the same framework that EventAddActivity does (same name EditText, same time TimePicker etc.)
+* have the user edit the framework.
+* delete the original event from firebase
+* add the updated event to firebase.
+*
+ */
+
+public class EventEditActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private EditText etName;
+    private EditText etDate;
+    private TimePicker startPicker;
+    private TimePicker endPicker;
+    private EditText etLocation;
+    private EditText etDescription;
+    private Button btnAdd;
+    private FirebaseFirestore db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event_edit);
+
+        etName = findViewById(R.id.etName);
+        etDate = findViewById(R.id.etDate);
+        startPicker = findViewById(R.id.startPicker);
+        endPicker = findViewById(R.id.endPicker);
+        etLocation = findViewById(R.id.etLocation);
+        etDescription = findViewById(R.id.etDescription);
+        btnAdd = findViewById(R.id.btnAdd);
+
+        db = FirebaseFirestore.getInstance();
+
+        //implement onclick listener for button
+        btnAdd.setOnClickListener(this);
+
+    } //method for data entry
+    private void dataEntry() {
+        int startHour = startPicker.getCurrentHour();
+        int startMin = startPicker.getCurrentMinute();
+        int endHour = endPicker.getCurrentHour();
+        int endMin = endPicker.getCurrentMinute();
+
+        String startTime = startHour + ":" + startMin;
+        String endTime = endHour + ":" + endMin;
+
+        //used for error messages
+        int start = (startHour * 60) + startMin;
+        int end = (endHour * 60) + endMin;
+
+
+        if(start > end) {
+            Toast.makeText(this, "End time is before start time",Toast.LENGTH_SHORT).show();
+        }
+        else if(start==end) {
+            Toast.makeText(this, "Times are the same", Toast.LENGTH_SHORT).show();
+        }
+        //if the user gets past all the errors then actual data will be entered in firebase
+        else {
+            //need a map to store the data
+            Map<String,String> eventInfo = new HashMap<>();
+            eventInfo.put("Name",etName.getText().toString().trim());
+            eventInfo.put("Date",etDate.getText().toString().trim());
+            eventInfo.put("startTime",startTime);
+            eventInfo.put("endTime",endTime);
+            eventInfo.put("Location",etLocation.getText().toString().trim());
+            eventInfo.put("Description",etDescription.getText().toString().trim());
+
+            //now put the map into the database
+            db.collection("Events").document(etName.getText().toString().trim()).set(eventInfo).addOnSuccessListener(new OnSuccessListener<Void>() {
+                @Override
+                public void onSuccess(Void aVoid) {
+                    Toast.makeText(EventEditActivity.this, "Event Added!", Toast.LENGTH_SHORT).show();
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Toast.makeText(EventEditActivity.this, "Unsuccessful.", Toast.LENGTH_SHORT).show();
+                }
+            });
+
+        }
+
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v == btnAdd){
+            dataEntry();
+        }
     }
 }
--- app/src/main/java/com/spicystar/jsoosu/EventsFragment.java
+++ app/src/main/java/com/spicystar/jsoosu/EventsFragment.java
@@ -11,26 +11,79 @@ import android.webkit.WebView;
 import android.widget.Button;
 import android.widget.LinearLayout;
 
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.sql.Array;
+import java.util.List;
+
 public class EventsFragment extends Fragment {
 
-    private LinearLayout linearLayout;
+    private LinearLayout linear;
+    private String[] eventsArray;
+    private FirebaseFirestore db;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_events, container, false);
 
-        linearLayout = v.findViewById(R.id.linearLayout);
+        linear = v.findViewById(R.id.linear);
 
+        db = FirebaseFirestore.getInstance();
+
+
+        db.collection("Events").get().addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+            @Override
+            public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+                if (!queryDocumentSnapshots.isEmpty()) {
+                    List<DocumentSnapshot> list = queryDocumentSnapshots.getDocuments();
+                    int i = 0;
+                    for (DocumentSnapshot d : list) {
+
+                        String date = d.getString("Date");
+                        buttons(date);
+
+                    }
+
+                }
+            }
+        });
 
         Button butt = new Button(getActivity());
-        butt.setText("Hello There");
         butt.setBackgroundColor(getResources().getColor(R.color.colorAccent));
         butt.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT));
-        linearLayout.addView(butt);
+        linear.addView(butt);
 
 
         return v;
+
+
+    }
+        private void buttons(final String date) {
+
+            //create the buttons to show up for each document
+            Button btn = new Button(this.getActivity());
+            btn.setText(date);
+            btn.setAllCaps(false);
+            linear.addView(btn);
+
+            btn.setOnLongClickListener(new View.OnLongClickListener() {
+                @Override
+                public boolean onLongClick(View v) {
+                    //removes button from view
+                    linear.removeView(v);
+                    //removes the data from database
+                    db.collection("Events").document(date).delete();
+                    return true;
+                }
+            });
+
+
+
+
 
 
     }
