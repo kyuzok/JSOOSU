b80175286b61639a892994c0781c6e3fb166d4fb
VisshaalS <40528586+visshaals@users.noreply.github.com>
2019-06-05 16:58:01 -0400

    Best datepicker in existence
    Signed-off-by: VisshaalS <40528586+VisshaalS@users.noreply.github.com>

--- app/src/main/java/com/spicystar/jsoosu/EventAddActivity.java
+++ app/src/main/java/com/spicystar/jsoosu/EventAddActivity.java
@@ -1,10 +1,12 @@
 package com.spicystar.jsoosu;
 
+import android.app.DatePickerDialog;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.TimePicker;
 import android.widget.Toast;
@@ -13,7 +15,10 @@ import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.firestore.FirebaseFirestore;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.HashMap;
+import java.util.Locale;
 import java.util.Map;
 
 public class EventAddActivity extends AppCompatActivity implements View.OnClickListener {
@@ -46,6 +51,35 @@ public class EventAddActivity extends AppCompatActivity implements View.OnClickL
         btnAdd.setOnClickListener(this);
 
 
+
+        final Calendar myCalendar = Calendar.getInstance();
+        final DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
+
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear,
+                                  int dayOfMonth) {
+                // TODO Auto-generated method stub
+                myCalendar.set(Calendar.YEAR, year);
+                myCalendar.set(Calendar.MONTH, monthOfYear);
+                myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                String myFormat = "MM/dd/yy"; //In which you need put here
+                SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
+
+                etDate.setText(sdf.format(myCalendar.getTime()));
+            }
+
+        };
+
+        etDate.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                new DatePickerDialog(EventAddActivity.this, date, myCalendar
+                        .get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
+                        myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
 
 
     }
@@ -104,10 +138,9 @@ public class EventAddActivity extends AppCompatActivity implements View.OnClickL
 
 
 
-
     @Override
     public void onClick(View v) {
-        if(v == btnAdd){
+        if(v == btnAdd) {
             dataEntry();
         }
     }
